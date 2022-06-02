ec13ed2e2e839265eaab41c1e0dfa61cf5632eb3
“TimLit13” <litidm@gmail.com>
2022-06-02 21:37:45 +0300

    determine current stock and changes

--- stock.rb
+++ stock.rb
@@ -1,9 +1,15 @@
-# STDIN.read.split('\n').each do |a|
-#   puts a
-# end
-
-
 input_data = STDIN.read.split("\n")
 
 stock_start_index = input_data.index('current_stock')
 stock_update_index = input_data.index('stock_update')
+
+current_stock = input_data[1...stock_update_index] 
+current_stock.each_with_index do |item, index|
+  current_stock[index] = item.split(',')
+  current_stock.delete_at(index) if current_stock[index].last == 'out of stock'
+end
+
+update_for_stock = input_data[(stock_update_index+1)..input_data.length] 
+update_for_stock.each_with_index do |item, index|
+  update_for_stock[index] = item.split(',')
+end
