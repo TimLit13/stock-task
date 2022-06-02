e5e60da5392b021a64bbac5ff1701bc85195dee1
“TimLit13” <litidm@gmail.com>
2022-06-02 22:00:40 +0300

    Logic for updating stock

--- stock.rb
+++ stock.rb
@@ -13,3 +13,22 @@ update_for_stock = input_data[(stock_update_index+1)..input_data.length]
 update_for_stock.each_with_index do |item, index|
   update_for_stock[index] = item.split(',')
 end
+
+update_for_stock.length.times do |i|
+  flag = false
+  current_stock.length.times do |j|
+    if update_for_stock[i][1] == current_stock[j][1] && update_for_stock[i][0].to_i > 0
+      if current_stock[j].last != 'full'
+        current_stock[j][0] = current_stock[j][0].to_i + update_for_stock[i][0].to_i
+        flag = true
+        break
+      else
+        flag = true
+        break
+      end
+    end
+  end
+  current_stock.push(update_for_stock[i]) unless flag
+end
+
+# puts current_stock.inspect
